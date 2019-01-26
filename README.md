f3835a6d7fbd1b6e29acbd0e27087d3d7b62e74d
Logan Ramos <lramos15@gmail.com>
2019-01-26 10:23:50 -0500

    Added ability to check which websites account the slack id was associated with

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Allows the linking of the Slack usernames to their associated RPI Ambulance Account
 
 To use first run npm install and then npm start.
-You will also have to configure Slack to utilize the command /linkuser @Slack Username RPIA Website ID
+You will also have to configure Slack to utilize the command /linkuser @Slack Username RPIA Website ID and /checkuser [@slack usernam]
+Note: If you do not turn on escape usernames for both commands this will not work!
 
 If you have any questions please feel free to reach out.
--- server.js
+++ server.js
@@ -51,57 +51,100 @@ app.listen(3000,function(){
 })
 
 function slackSlashCommands(req, res){
-    var req_body = req.body;
-    var command = req_body.command;
+    var command = req.body.command;
     // Link user command
     if(command == "/linkuser"){
-        var slack_userinfo_url = "https://slack.com/api/users.info?token=" + slackAccessToken + "&user=" + req_body.user_id;
-        request.get(slack_userinfo_url, function(error, resp, bod){
-            if (!error && resp.statusCode == 200) {
-                bod = JSON.parse(bod);
-                // If the user isn't an admin don't go any further
-                if(!(bod.user.is_admin)){
-                    return res.send("This command can only be used by an admin!");
-                }else{
-                    var text = req_body.text
-                    text = text.split(" ");
-                    var user = text[0];
-                    // Means it's most likely not a user
-                    if(user.indexOf("<") != 0){
-                        res.send("The first paramter must be a user!");
-                        return;
-                    }
-            
-                    user = user.substring(0,user.indexOf("|"));
-                    user += ">";
-                    var web_id = text[1];
-                    if(isNaN(web_id)){
-                        res.send("The second paramater must be a whole number!");
-                        return;
-                    }
-            
-                    // Sends the post request to rpiambulance.com 
-                    request.post({
-                        url:'https://rpiambulance.com/slack-link.php', 
-                        form: {slack_id: user, member_id: web_id}
-                    }, function(err,response,body){
-                        if (!err && response.statusCode == 200) {
-                            return res.send(body);
-                        }else{
-                            var message = "Oops! Something happened with that server request, please try again later.";
-                            return res.send(message);
-                        }
-                        
-                    });
-                }
-            }else{
-                var message = "Oops! Something happened with that server request, please try again later.";
-                return res.send(message);
-            }
-        });
+        linkUser(req,res);
+    }else if(command == "/checklink"){
+        // I felt the code was getting a little cluttered so I moved the command into a function
+        checkUserLink(req,res);
     }else{
         // This gets hit if slack sends a post to this app but we didn't program for that command
         console.log("Command not configured!");
         res.send("It appears the command you are trying to send isn't support");
     }
+}
+
+function checkUserLink(req, res){
+    var req_body = req.body;
+    // The paramaters after the command
+    var text = req_body.text;
+    // Puts it in the format that database has
+    var user = "<@" + req_body.user_id + ">";
+    var rpia_query_url = "https://rpiambulance.com/slack-link.php?slack_id=";
+    // If they gave us no paramaters just return themselves
+    if(text.length == 0){
+        // Encodes the userID of who initialized the command to be sent
+        rpia_query_url += encodeURIComponent(user);
+    }else{
+        text = text.split(" ");
+        user = text[0];
+        console.log(user);
+        // Means it's most likely not a user
+        if(user.indexOf("<") != 0){
+            res.send("The first paramter must be a user!");
+            return;
+        }
+        // We chop off the username as slack is deprecating this
+        user = user.substring(0,user.indexOf("|"));
+        user += ">";
+        rpia_query_url += encodeURI(user);
+    }
+    // Here's where we make the actual request to RPIA servers
+    request.get(rpia_query_url, function(error,resp,body){
+        if(!error && resp.statusCode == 200){
+            // Whatever the website returns is the message we will use.
+            return res.send(body);
+        }else{
+            return res.send("Oops! Something went wrong with the server request to RPIA!");
+        }
+    });
+}
+
+function linkUser(req,res){
+    var req_body = req.body;
+    var slack_userinfo_url = "https://slack.com/api/users.info?token=" + slackAccessToken + "&user=" + req_body.user_id;
+    request.get(slack_userinfo_url, function(error, resp, bod){
+        if (!error && resp.statusCode == 200) {
+            bod = JSON.parse(bod);
+            // If the user isn't an admin don't go any further
+            if(!(bod.user.is_admin)){
+                return res.send("This command can only be used by an admin!");
+            }else{
+                var text = req_body.text
+                text = text.split(" ");
+                var user = text[0];
+                // Means it's most likely not a user
+                if(user.indexOf("<") != 0){
+                    res.send("The first paramter must be a user!");
+                    return;
+                }
+        
+                user = user.substring(0,user.indexOf("|"));
+                user += ">";
+                var web_id = text[1];
+                if(isNaN(web_id)){
+                    res.send("The second paramater must be a whole number!");
+                    return;
+                }
+        
+                // Sends the post request to rpiambulance.com 
+                request.post({
+                    url:'https://rpiambulance.com/slack-link.php', 
+                    form: {slack_id: user, member_id: web_id}
+                }, function(err,response,body){
+                    if (!err && response.statusCode == 200) {
+                        return res.send(body);
+                    }else{
+                        var message = "Oops! Something happened with that server request, please try again later.";
+                        return res.send(message);
+                    }
+                    
+                });
+            }
+        }else{
+            var message = "Oops! Something happened with that server request, please try again later.";
+            return res.send(message);
+        }
+    });
 }
